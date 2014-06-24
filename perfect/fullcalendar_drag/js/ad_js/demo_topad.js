3586a4fa2b9c35f093ca7211f3c185c701b21d58
JKAvatar <pancheng9126@126.com>
2014-06-24 15:58:37 +0800

    calendar
    http://www.helloweba.com/demo/fullcalendar/drag.html

---
+++ perfect/fullcalendar_drag/js/ad_js/demo_topad.js
@@ -0,0 +1,2 @@
+document.write("<script type=\"text/javascript\">google_ad_client = \"ca-pub-7515443544894528\";google_ad_slot = \"3076591508\";google_ad_width = 468;google_ad_height = 60;</script>");
+document.write("<script type=\"text/javascript\" src=\"//pagead2.googlesyndication.com/pagead/show_ads.js\"></script>");
